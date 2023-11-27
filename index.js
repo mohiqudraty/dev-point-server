9e764c78efdbb1f6b39414f3fc38039d4ea432ae
mohiqudraty <mohi.qudraty@gmail.com>
2023-11-27 23:00:57 +0600

    make all users api

--- index.js
+++ index.js
@@ -32,16 +32,88 @@ async function run() {
     const announcementCollection = client
       .db("devPointDB")
       .collection("announcement");
+    const userCollection = client
+      .db("devPointDB")
+      .collection("users");
+
+
+
+
+
+// users api ----------------------------------
+app.post('/users', async (req, res) => {
+  const user = req.body
+
+const query = {email: user.email}
+const existUser = await userCollection.findOne(query)
+if(existUser){
+  return  res.send({message: "User Already Exist Please Login", insertedId: null})
+}
+
+  const result = await userCollection.insertOne(user)
+  res.send(result)
+})
+
+// user get api -----------------------
+app.get('/users', async (req, res) => {
+  const email = req.query.email
+  console.log(email);
+  const query = {email: email}
+  const result = await userCollection.findOne(query)
+  res.send(result)
+})
+// get all user api -----------
+app.get('/all-users', async (req, res) => {
+  const result = await userCollection.find().toArray()
+  res.send(result)
+})
+
+
+    // voting ================================
+    // api  for up voting a post-------------------
+    app.put("/posts/:id/upvote", async (req, res) => {
+      const { id } = req.params;
+
+      try {
+        const result = await postCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $inc: { upVote: 1 } }
+        );
+        res.send(result);
+      } catch (error) {
+        console.error("Error up voting post:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Error up voting post" });
+      }
+    });
+    // api  for up down voting a post-----------------------
+    app.put("/posts/:id/downvote", async (req, res) => {
+      const { id } = req.params;
+
+      try {
+        const result = await postCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $inc: { downVote: 1 } }
+        );
+        res.send(result);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ success: false, message: "Error down voting post" });
+      }
+    });
 
     // post api=================================
-
+    // add post ----
     app.post("/add-post", async (req, res) => {
       const post = req.body;
       console.log(post);
-      const result = await postCollection.insertOne(post);
-      res.send(result);
+      const addPost = await postCollection.insertOne(post);
+      res.send(addPost);
     });
 
+    // all post and searched post api --------------
     app.get("/all-post", async (req, res) => {
       try {
         const tag = req.query.tag;
@@ -49,13 +121,34 @@ async function run() {
         if (tag) {
           query = { tag: { $regex: new RegExp(tag, "i") } };
         }
-        const result = await postCollection
+        const allPost = await postCollection
           .find(query)
           .sort({ postedTime: -1 })
           .toArray();
-        res.send(result);
+        res.send(allPost);
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error(error);
+      }
+    });
+    // all sorted post api -----------------
+    app.get("/sorted-posts", async (req, res) => {
+      try {
+        const sortedPosts = await postCollection
+          .aggregate([
+            {
+              $addFields: {
+                voteDifference: { $subtract: ["$upVote", "$downVote"] },
+              },
+            },
+            {
+              $sort: { voteDifference: -1 },
+            },
+          ])
+          .toArray();
+
+        res.send(sortedPosts);
+      } catch (error) {
+        res.status(500).json({ message: "Error fetching sorted posts" });
       }
     });
 
@@ -63,20 +156,20 @@ async function run() {
     app.get("/single-post/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await postCollection.findOne(query);
-      res.send(result);
+      const post = await postCollection.findOne(query);
+      res.send(post);
     });
 
     // announcement api -------------
     app.get("/announcements", async (req, res) => {
-      const result = await announcementCollection.find().toArray();
-      res.send(result);
+      const announcement = await announcementCollection.find().toArray();
+      res.send(announcement);
     });
 
     // membership data ---------------------------------
     app.get("/membership", async (req, res) => {
-      const result = await membershipCollection.find().toArray();
-      res.send(result);
+      const membership = await membershipCollection.find().toArray();
+      res.send(membership);
     });
 
     // Send a ping to confirm a successful connection
